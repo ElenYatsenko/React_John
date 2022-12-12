8b6340987863aee966b275df1eef76263caa5931
Elena <15el.shev@gmail.com>
2022-12-12 23:00:17 +0200

    Publish app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header/index.js";
 import "./index.css";
+import Home from "./components/Hello/index.js";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
+      {/* <Home /> */}
     </BrowserRouter>
   );
 }
